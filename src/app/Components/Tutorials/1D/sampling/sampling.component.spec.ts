9fe155aa02a0974af2c041cbb7de53909f3a2edc
ClementPla <clement.playout@polymtl.ca>
2025-09-30 17:51:26 -0400

    Trying to fix some stuff

---
+++ src/app/Components/Tutorials/1D/sampling/sampling.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SamplingComponent } from './sampling.component';
+
+describe('SamplingComponent', () => {
+  let component: SamplingComponent;
+  let fixture: ComponentFixture<SamplingComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+    declarations: [SamplingComponent]
+})
+    .compileComponents();
+
+    fixture = TestBed.createComponent(SamplingComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
